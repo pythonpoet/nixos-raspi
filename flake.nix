9f091ef8b0d0719508f5add447a7a25f19106267
Sergey Kazenyuk <kazenyuk@pm.me>
2025-04-20 05:10:44 +0300

    flake: use nixos-raspberrypi.lib.nixosSystem to create configurations

--- flake.nix
+++ flake.nix
@@ -54,163 +54,121 @@
     });
 
     nixosConfigurations = let
-
-      mkNixOSRPi = let
-        # use `nixpkgs` used by nixos-raspberrypi
-        # to be able to benefit from its cachix cache
-        nixpkgs = self.inputs.nixos-raspberrypi.inputs.nixpkgs;
-      in moreModules: nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        specialArgs = inputs;
-        modules = [
-          nixos-raspberrypi-config
-        ] ++ moreModules;
-      };
-
-      nixos-raspberrypi-config = { config, ... }: {
-        imports = with nixos-raspberrypi.nixosModules; [
-          # Nix cache with prebuilt packages,
-          # see `devshells/nix-build-to-cachix.nix` for a list
-          trusted-nix-caches
-
-          # All RPi and RPi-optimised packages to be available in `pkgs.rpi`
-          nixpkgs-rpi
-          # Add necessary overlays with kernel, firmware, vendor packages
-          nixos-raspberrypi.lib.inject-overlays
-          # Optonally add overlays with optimised packages into the global scope
-          nixos-raspberrypi.lib.inject-overlays-global
-        ];
-
-        system.nixos.tags = let
-          cfg = config.boot.loader.raspberryPi;
-        in[
-          "raspberry-pi-${cfg.variant}"
-          cfg.bootloader
-          config.boot.kernelPackages.kernel.version
-        ];
-      };
-
-      installer-config = ({ config, pkgs, lib, modulesPath, ... }: {
-        # /installer/sd-card/sd-image-aarch64-installer.nix
-        imports = [
-          (modulesPath + "/profiles/installation-device.nix")
-        ];
-
-        # disable swraid – it breaks the boot on raspberry:
-        # - rootfs image is not initramfs (write error): looks like initrd
-        # - /initrd.image: incomplete write (-28 != 25571065)
-        # with the subsequent boot failure
-        boot.swraid.enable = lib.mkForce false;
-
-        # the installation media is also the installation target,
-        # so we don't want to provide the installation configuration.nix.
-        installer.cloneConfig = false;
-      });
-
       common-user-config = {config, ... }: {
+        imports = [ ./modules/nice-looking-console.nix ];
+
         time.timeZone = "UTC";
         networking.hostName = "rpi${config.boot.loader.raspberryPi.variant}-demo";
 
         services.udev.extraRules = ''
           # Ignore partitions with "Required Partition" GPT partition attribute
-          # On our RPi's this is firmware (/boot/firmware) partition
+          # On our RPis this is firmware (/boot/firmware) partition
           ENV{ID_PART_ENTRY_SCHEME}=="gpt", \
             ENV{ID_PART_ENTRY_FLAGS}=="0x1", \
             ENV{UDISKS_IGNORE}="1"
         '';
+
+        system.nixos.tags = let
+          cfg = config.boot.loader.raspberryPi;
+        in [
+          "raspberry-pi-${cfg.variant}"
+          cfg.bootloader
+          config.boot.kernelPackages.kernel.version
+        ];
       };
     in {
 
-      rpi02 = mkNixOSRPi [
-        ({ config, pkgs, lib, nixos-raspberrypi, ... }: {
-          imports = with nixos-raspberrypi.nixosModules; [
-            # Hardware configuration
-            raspberry-pi-02.base
-            usb-gadget-ethernet
-            # SD-Card image
-            # sd-image-uboot
-            # config.txt example
-            ./pi02-configtxt.nix
-
-            ./modules/nice-looking-console.nix
-          ];
-        })
-        installer-config
-        common-user-config
-        ({ config, pkgs, ... }: {
-          fileSystems = {
-            "/boot/firmware" = {
-              device = "/dev/disk/by-label/FIRMWARE";
-              fsType = "vfat";
-              options = [
-                "noatime"
-                "noauto"
-                "x-systemd.automount"
-                "x-systemd.idle-timeout=1min"
-              ];
+      rpi02 = nixos-raspberrypi.lib.nixosSystemFull {
+        specialArgs = inputs;
+        modules = [
+          ({ config, pkgs, lib, nixos-raspberrypi, ... }: {
+            imports = with nixos-raspberrypi.nixosModules; [
+              # Hardware configuration
+              raspberry-pi-02.base
+              usb-gadget-ethernet
+              # config.txt example
+              ./pi02-configtxt.nix
+            ];
+          })
+          # Disk configuration
+          # Assumes the system will continue to reside on the installation media (sd-card),
+          # as there're hardly other feasible options on RPi02.
+          # (see also https://github.com/nvmd/nixos-raspberrypi/issues/8#issuecomment-2804912881)
+          # `sd-image` has lots of dependencies unnecessary for the installed system,
+          # replicating its disk layout
+          ({ config, pkgs, ... }: {
+            fileSystems = {
+              "/boot/firmware" = {
+                device = "/dev/disk/by-label/FIRMWARE";
+                fsType = "vfat";
+                options = [
+                  "noatime"
+                  "noauto"
+                  "x-systemd.automount"
+                  "x-systemd.idle-timeout=1min"
+                ];
+              };
+              "/" = {
+                device = "/dev/disk/by-label/NIXOS_SD";
+                fsType = "ext4";
+                options = [ "noatime" ];
+              };
             };
-            "/" = {
-              device = "/dev/disk/by-label/NIXOS_SD";
-              fsType = "ext4";
-              options = [ "noatime" ];
-            };
-          };
+          })
+          # Further user configuration
+          common-user-config
+          ({ config, pkgs, ... }: {
+            environment.systemPackages = with pkgs; [
+              tree
+              i2c-tools
+            ];
+          })
+        ];
+      };
 
-          environment.systemPackages = with pkgs; [
-            tree
-            raspberrypi-eeprom
-            i2c-tools
-          ];
-        })
-      ];
+      rpi4 = nixos-raspberrypi.lib.nixosSystem {
+        specialArgs = inputs;
+        modules = [
+          ({ config, pkgs, lib, nixos-raspberrypi, disko, ... }: {
+            imports = with nixos-raspberrypi.nixosModules; [
+              # Hardware configuration
+              raspberry-pi-4.base
+              raspberry-pi-4.display-vc4
+              raspberry-pi-4.bluetooth
+            ];
+          })
+          # Disk configuration
+          disko.nixosModules.disko
+          ./disko-usb-btrfs.nix
+          # Further user configuration
+          common-user-config
+          {
+            boot.tmp.useTmpfs = true;
+          }
+        ];
+      };
 
-      rpi4 = mkNixOSRPi [
-        ({ config, pkgs, lib, nixos-raspberrypi, disko, ... }: {
-          imports = with nixos-raspberrypi.nixosModules; [
-            # Hardware configuration
-            raspberry-pi-4.base
-            raspberry-pi-4.display-vc4
-            raspberry-pi-4.bluetooth
-
-            # SD-Card image
-            # sd-image-uboot
-            disko.nixosModules.disko
-            ./disko-usb-btrfs.nix
-
-            ./modules/nice-looking-console.nix
-          ];
-        })
-        installer-config
-        common-user-config
-        {
-          boot.tmp.useTmpfs = true;
-        }
-      ];
-
-      rpi5 = mkNixOSRPi [
-        ({ config, pkgs, lib, nixos-raspberrypi, disko, ... }: {
-          imports = with nixos-raspberrypi.nixosModules; [
-            # Hardware configuration
-            raspberry-pi-5.base
-            raspberry-pi-5.display-vc4
-            raspberry-pi-5.bluetooth
-
-            # SD-Card image
-            # sd-image-kernelboot
-            disko.nixosModules.disko
-            ./disko-usb-btrfs.nix
-            # ./disko-nvme-zfs.nix
-            # { networking.hostId = "8821e309"; } # for zfs
-
-            ./modules/nice-looking-console.nix
-          ];
-        })
-        installer-config
-        common-user-config
-        {
-          boot.tmp.useTmpfs = true;
-        }
-      ];
+      rpi5 = nixos-raspberrypi.lib.nixosSystemFull {
+        specialArgs = inputs;
+        modules = [
+          ({ config, pkgs, lib, nixos-raspberrypi, disko, ... }: {
+            imports = with nixos-raspberrypi.nixosModules; [
+              # Hardware configuration
+              raspberry-pi-5.base
+              raspberry-pi-5.display-vc4
+            ];
+          })
+          # Disk configuration
+          disko.nixosModules.disko
+          ./disko-nvme-zfs.nix
+          { networking.hostId = "8821e309"; } # for zfs
+          # Further user configuration
+          common-user-config
+          {
+            boot.tmp.useTmpfs = true;
+          }
+        ];
+      };
 
     };
 
