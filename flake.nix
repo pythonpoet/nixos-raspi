89816dcd0f783d0c02b984e7b62f3fdfa6926d82
Sergey Kazenyuk <kazenyuk@pm.me>
2025-04-20 07:02:50 +0300

    rpi5: add example of the custom kernel configuration

--- flake.nix
+++ flake.nix
@@ -233,6 +233,29 @@
           {
             boot.tmp.useTmpfs = true;
           }
+
+          # Advanced: Use non-default kernel from kernel-firmware bundle
+          ({ config, pkgs, lib, ... }: let
+            kernelBundle = pkgs.linuxAndFirmware.v6_6_31;
+          in {
+            boot = {
+              loader.raspberryPi.firmwarePackage = kernelBundle.raspberrypifw;
+              kernelPackages = kernelBundle.linuxPackages_rpi5;
+            };
+
+            nixpkgs.overlays = lib.mkAfter [
+              (self: super: {
+                # This is used in (modulesPath + "/hardware/all-firmware.nix") when at least 
+                # enableRedistributableFirmware is enabled
+                # I know no easier way to override this package
+                inherit (kernelBundle) raspberrypiWirelessFirmware;
+                # Some derivations want to use it as an input,
+                # e.g. raspberrypi-dtbs, omxplayer, sd-image-* modules
+                inherit (kernelBundle) raspberrypifw;
+              })
+            ];
+          })
+
         ];
       };
 
