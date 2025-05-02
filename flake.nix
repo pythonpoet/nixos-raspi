c7d358447a02d10e6b9526ea26254d5d3024f403
Sergey Kazenyuk <kazenyuk@pm.me>
2025-05-03 02:42:46 +0300

    flake: provide base network config with iwd to enable easier wifi connectivity

--- flake.nix
+++ flake.nix
@@ -103,12 +103,49 @@
         # We are stateless, so just default to latest.
         system.stateVersion = config.system.nixos.release;
       });
+
+      network-config = {
+        # This is mostly portions of safe network configuration defaults that
+        # nixos-images and srvos provide
+
+        networking.useNetworkd = true;
+        # mdns
+        networking.firewall.allowedUDPPorts = [ 5353 ];
+        systemd.network.networks = {
+          "99-ethernet-default-dhcp".networkConfig.MulticastDNS = "yes";
+          "99-wireless-client-dhcp".networkConfig.MulticastDNS = "yes";
+        };
+
+        # This comment was lifted from `srvos`
+        # Do not take down the network for too long when upgrading,
+        # This also prevents failures of services that are restarted instead of stopped.
+        # It will use `systemctl restart` rather than stopping it with `systemctl stop`
+        # followed by a delayed `systemctl start`.
+        systemd.services = {
+          systemd-networkd.stopIfChanged = false;
+          # Services that are only restarted might be not able to resolve when resolved is stopped before
+          systemd-resolved.stopIfChanged = false;
+        };
+
+        # Use iwd instead of wpa_supplicant. It has a user friendly CLI
+        networking.wireless.enable = false;
+        networking.wireless.iwd = {
+          enable = true;
+          settings = {
+            Network = {
+              EnableIPv6 = true;
+              RoutePriorityOffset = 300;
+            };
+            Settings.AutoConnect = true;
+          };
+        };
       };
 
       common-user-config = {config, pkgs, ... }: {
         imports = [
           ./modules/nice-looking-console.nix
           users-config-stub
+          network-config
         ];
 
         time.timeZone = "UTC";
