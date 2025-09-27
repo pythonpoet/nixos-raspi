0637fd953da29c7a98aa679f8c61ee229f71290a
Sergey Kazenyuk <kazenyuk@pm.me>
2025-09-27 13:08:41 +0300

    rpi5: enable page-size-16k

--- flake.nix
+++ flake.nix
@@ -261,6 +261,7 @@
             imports = with nixos-raspberrypi.nixosModules; [
               # Hardware configuration
               raspberry-pi-5.base
+              raspberry-pi-5.page-size-16k
               raspberry-pi-5.display-vc4
               ./pi5-configtxt.nix
             ];
